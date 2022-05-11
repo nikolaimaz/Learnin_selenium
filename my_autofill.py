37f86981044f29e8076f582db632844fa491e798
Nikolai <69244449+nikolaimaz@users.noreply.github.com>
2022-05-11 15:15:51 +0200

    Update my_autofill.py

--- my_autofill.py
+++ my_autofill.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 import time
 import pytest
 
@@ -11,38 +11,45 @@ options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0
 options.add_argument("--disable-blink-features=AutomationControlled")
 
 
-url = "https://mikolai-mazurek.tk/"
+url = "http://suninjuly.github.io/simple_form_find_task.html"
 
-driver = webdriver.Firefox(service=Service('/home/nikolai/PycharmProjects/test/geckodriver'),
+driver = webdriver.Chrome(executable_path='SET PATH FOR DRIVER ',
                           options=options)
 
 
 # filling form
-
-def test():
+def filling():
     try:
         driver.get(url=url)
-        time.sleep(1)
+        time.sleep(10)
 
-        search_input= driver.find_element(by=By.NAME, value='name')
+        search_input= driver.find_element(by=By.NAME, value='first_name')
         search_input.send_keys('My name is  Slim Shady')
-        print('done')
 
+        search_input = driver.find_element(by=By.NAME, value='last_name')
+        search_input.send_keys('The real Slim Shady')
 
-
-
-        search_input = driver.find_element(by=By.ID, value='message')
+        search_input = driver.find_element(by=By.NAME, value='firstname')
         search_input.send_keys('Detroit')
-        print('done')
 
+        search_input = driver.find_element(by=By.ID, value='country')
+        search_input.send_keys('USA')
 
+        search_input = driver.driver.find_element(by=By.ID, value='submit_button')
+        search_input.click()
 
+        alert = driver.switch_to.alert
+        alert_text = alert.text
+        print(alert_text)
+        time.sleep(2)
+        alert.accept()
+
+        time.sleep(2)
 
     except Exception as ex:
         print(ex)
 
     finally:
-        time.sleep(10)
         driver.close()
         driver.quit()
-test()
+filling()
